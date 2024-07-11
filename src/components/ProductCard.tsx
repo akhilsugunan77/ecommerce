f890f9ebe745b7cfb628643805201700f70adfd3
akhilsugunan77 <104181198+akhilsugunan77@users.noreply.github.com>
2024-07-11 11:52:06 +0530

    refactor: product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,24 +10,25 @@ type ProductCardProps = Pick<
 export function ProductCard(props: ProductCardProps) {
   const { id, title, thumbnail, price, rating } = props;
   return (
-    <Link href={`/product/${id}`}>
-      <div className="rounded-3xl overflow-hidden bg-white shadow-sm h-full">
-        <Image
-          src={thumbnail}
-          alt={title}
-          width={256}
-          height={256}
-          className="mx-auto object-contain"
-        />
-        <div className="p-6 flex flex-col justify-between">
-          <h4 className="text-sm font-semibold mb-1">{title}</h4>
-          <div className="flex gap-0.5 mb-2">
-            {Array.from({ length: rating }).map((_, i) => (
-              <Star key={i} />
-            ))}
-          </div>
-          <span className="text-xl font-medium">${price}</span>
+    <Link
+      href={`/product/${id}`}
+      className="rounded-3xl overflow-hidden bg-white shadow-sm grow flex flex-col"
+    >
+      <Image
+        src={thumbnail}
+        alt={title}
+        width={256}
+        height={256}
+        className="mx-auto object-contain"
+      />
+      <div className="px-6 pb-4 pt-2 flex flex-col justify-between grow">
+        <h4 className="text-sm font-semibold mb-1">{title}</h4>
+        <div className="flex gap-0.5 mb-2">
+          {Array.from({ length: Math.floor(rating) }).map((_, i) => (
+            <Star key={i} />
+          ))}
         </div>
+        <span className="text-xl font-medium">${price}</span>
       </div>
     </Link>
   );
