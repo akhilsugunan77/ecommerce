e5dfe375f3ba002c3491df263ad2bc86a55c8212
akhilsugunan77 <104181198+akhilsugunan77@users.noreply.github.com>
2024-07-11 12:37:40 +0530

    refactor: resue path

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,14 +1,16 @@
 import { Product } from "./type";
 
+const DATA_PATH = "http://localhost:3000/data/products.json"
+
 export async function getProducts() {
-  const response = await fetch("http://localhost:300/data/products.json");
+  const response = await fetch(DATA_PATH);
   const productListData = await response.json();
   const { products } = productListData;
   return products as Product[];
 }
 
 export async function getProductDetails(productId: number) {
-  const response = await fetch("http://localhost:300/data/products.json");
+  const response = await fetch(DATA_PATH);
   const productListData = await response.json();
   const { products } = productListData;
   const product = (products as Product[]).find(
